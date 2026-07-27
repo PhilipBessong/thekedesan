8d48f6207c53c32c2948359ede67332e59f4c12d
PhilipBessong <76492088+PhilipBessong@users.noreply.github.com>
2026-07-27 12:38:34 +0200

    .

--- thekedesan/src/app/app.routes.ts
+++ thekedesan/src/app/app.routes.ts
@@ -3,22 +3,62 @@ import { Home } from './home/home';
 import { Rooms } from './rooms/rooms';
 import { RoomDetails } from './room-details/room-details';
 import { Amenities } from './amenities/amenities';
-import {Aboutus} from './aboutus/aboutus';
-import {Faqs} from './faqs/faqs';
+import { Aboutus } from './aboutus/aboutus';
+import { Faqs } from './faqs/faqs';
 import { Policyterms } from './policyterms/policyterms';
 import { AmenitiesDetails } from './amenities-details/amenities-details';
 import { AppVirtualTour } from './app-virtual-tour/app-virtual-tour';
 import { Ktable } from './ktable/ktable';
 export const routes: Routes = [
-  { path: 'home', component: Home },
-  { path: 'rooms', component: Rooms },
-  { path: 'rooms/:id', component: RoomDetails },
-  { path: 'amenities', component: Amenities },
-  { path: 'amenities/:id', component: AmenitiesDetails },
-  { path: 'aboutus', component: Aboutus },
-  { path: 'faqs', component: Faqs },
-  { path: 'policy-terms', component: Policyterms },
-  { path: 'virtual-tour', component: AppVirtualTour },
-  { path: 'the-kedesan-table', component: Ktable }
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', title: 'The Kedesan Huis', component: Home },
+  {
+    path: 'rooms',
+    title: 'The Kedesan | Accommodations & Suites',
+    component: Rooms,
+  },
+  {
+    path: 'rooms/:id',
+    title: 'The Kedesan | Room Details',
+    component: RoomDetails,
+  },
+  {
+    path: 'amenities',
+    title: 'The Kedesan | Amenities & Guest Services',
+    component: Amenities,
+  },
+  {
+    path: 'amenities/:id',
+    title: 'The Kedesan | Amenity Details',
+    component: AmenitiesDetails,
+  },
+  {
+    path: 'aboutus',
+    title: 'The Kedesan | About Our Retreat',
+    component: Aboutus,
+  },
+  {
+    path: 'faqs',
+    title: 'The Kedesan | Frequently Asked Questions',
+    component: Faqs,
+  },
+  {
+    path: 'policy-terms',
+    title: 'The Kedesan | Policy & Terms',
+    component: Policyterms,
+  },
+  {
+    path: 'virtual-tour',
+    title: 'The Kedesan | Virtual Tour',
+    component: AppVirtualTour,
+  },
+  {
+    path: 'the-kedesan-table',
+    title: 'The Kedesan | The Kedesan Table',
+    component: Ktable,
+  },
 ];
